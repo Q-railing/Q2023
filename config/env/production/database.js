010f07cf9111abc31a6a1a31ad004ad2b5ce4169
René Hartjes <rene.hartjes@q-railing.com>
2023-04-26 08:38:30 +0200

    next try

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,18 +1,19 @@
     // path: ./config/env/production/database.js
 
     module.exports = ({ env }) => ({
+      connection: {
+        client: 'postgres',
         connection: {
-          client: 'postgres',
-          connection: {
-            host: env('DATABASE_HOST', '127.0.0.1'),
-            port: env.int('DATABASE_PORT', 5432),
-            database: env('DATABASE_NAME', 'strapi'),
-            user: env('DATABASE_USERNAME', 'strapi'),
-            password: env('DATABASE_PASSWORD', 'public'),
-            ssl: {
-              ca: env('DATABASE_CA')
-            },
+          host: env('DATABASE_HOST', 'localhost'),
+          port: env.int('DATABASE_PORT', 5432),
+          database: env('DATABASE_NAME', 'bank'),
+          user: env('DATABASE_USERNAME', 'postgres'),
+          password: env('DATABASE_PASSWORD', '0000'),
+          schema: env('DATABASE_SCHEMA', 'public'), // Not required
+          ssl: {
+            ca: env('DATABASE_CA'),
           },
-          debug: false,
         },
-      });
+        debug: false,
+      },
+    });
